c722b6cec8111479d502c59c1a8e131c7c953fb7
Dmitry Mironov <bitbucket@novorado.com>
2020-12-07 13:11:20 -0800

    Loading video with OpenCV for analysis

--- video_file.py
+++ video_file.py
@@ -1,6 +1,7 @@
 import numpy as np
 import sqlite3
 import os
+import cv2
 
 class DashamDatasetLoader:
     db=os.environ['HOME']+'/.dashcam.software/dashcam.index'
@@ -45,12 +46,34 @@ class DashamDatasetLoader:
     def __del__(self):
         self.connection.close()
 
+#
+# Use OpenCV to grab frames and build a time sequence
+#
+class FrameGenerator:
+    cap = None
+    frameCount = 0
+
+    def __init__(self,fn):
+        self.cap = cv2.VideoCapture(fn)
+        self.frameCount = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        self.W = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        self.H = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        self.FPS = int(self.cap.get(cv2.CAP_PROP_FPS))
+        print("Loaded '{}' {}x{} {}fps video".format(fn,self.W,self.H,self.FPS))
+
+    def __del__(self):
+        del self.cap
+
 def main():
     os.system('clear') # clear the terminal on linux
+    #   
+    # Get the file and load it's spatial and temporal ground truth
     loader = DashamDatasetLoader()
     file_name, trajectory=loader.next()
     del loader # free memory
-    print(trajectory.shape)
+    #
+    # Use OpenCV to load frame sequence and video temporal charateristics
+    framer = FrameGenerator(file_name)
 
 if __name__ == "__main__":
     main()
