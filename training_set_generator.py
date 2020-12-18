79705acf39a9cea58d9e51f9e77fbb11a2889e3d
Dmitry Mironov <bitbucket@novorado.com>
2020-12-17 22:12:07 -0800

    fixing img truncation

--- training_set_generator.py
+++ training_set_generator.py
@@ -41,6 +41,7 @@ class TrainingSetGenerator:
             assert img.shape == (1080,1920,3), "Unexpected garmin dimensions"
             # Crop to particular format (remove text)
             img = self.crop(L.file_type, img, target_dim)
+            cv2.imshow('1',img)
             # thats really weird but resize wants reversed HxW dimensions?
             out_image = cv2.resize(img, (target_dim[1],target_dim[0]))
             # update channels for the position
@@ -54,8 +55,10 @@ class TrainingSetGenerator:
                 prevgray=gray
             self.optical_flow[Tidx] = cv2.calcOpticalFlowFarneback(
                 prevgray, gray, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+            prevgray=gray
             # increment time dimension
             Tidx += 1
+            ch = 0xFF & cv2.waitKey(1)
 
     #
     # Methods
@@ -64,16 +67,19 @@ class TrainingSetGenerator:
     def crop(self, file_type, img, target_dim):
         assert file_type == 'garmin', 'unsupported file type ' + file_type
         if file_type == 'garmin':
-            # target aspect ratio
+            # target aspect ratio, W/H
             ar = target_dim[0]/target_dim[1]
+            # image size for opencv is H x W
             sz = img.shape
-            new_sz = [sz[0],sz[1]-50]
-            if new_sz[0]/new_sz[1] > ar:
-                # truncate width
-                new_sz[0]=int(new_sz[1]*ar)
+            Htxt=50
+            new_sz = [sz[0]-Htxt, sz[1]]
+            if new_sz[1]/new_sz[0] < ar:
+                # truncate height
+                new_sz[0] = int(new_sz[1]/ar)
+                dw = [int((sz[0]-new_sz[0])/2), 0]
             else:
-                # turncate height
-                new_sz[1]=int(new_sz[0]/ar)
-            dw = [int((sz[0]-new_sz[0])/2), int((sz[1]-new_sz[1])/2)]
-            return img[dw[0]:sz[0]-dw[0],dw[1]:sz[1]-dw[1]]
+                # turncate width
+                new_sz[1] = int(new_sz[0]*ar)
+                dw = [Htxt, int((sz[1]-new_sz[1])/2)]
+            return img[0:sz[0]-dw[0], 0:sz[1]-dw[1]]
         return img
