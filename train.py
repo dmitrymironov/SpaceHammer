625ed5e1e0a35cb16084214b3b1def617d68864c
Dmitry Mironov <dmitry.mironov@novorado.com>
2020-12-28 21:55:48 -0800

    Enabling GPU memory growth to fix cuDNN issue

--- train.py
+++ train.py
@@ -19,7 +19,23 @@ def main():
     else:
         db = os.environ['HOME']+'/.dashcam.software/dashcam.index'
     db = os.path.normpath(db)
+
+    '''
+    Addressing CUDA/cuDNN driver issues on Windows
+    '''
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if gpus:
+        try:
+            # Currently, memory growth needs to be the same across GPUs
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+        except RuntimeError as e:
+            # Memory growth must be set before GPUs have been initialized
+            print(e)
     
+    print('================================================== TRAIN')
     '''
     Generators
     '''
